888d033e2e1c312acdc002b1e80b13ddbe731532
SebasGlez03 <sebas.glez18040465@gmail.com>
2025-12-27 15:28:41 -0700

    UserDAO class created

---
+++ MosquiredSGV/SGV_Persistence/src/main/java/com/persistence/impl/UserDAO.java
@@ -0,0 +1,124 @@
+package com.persistence.impl;
+
+import java.util.List;
+
+import com.domain.User;
+import com.domain.util.JPAUtil;
+import com.persistence.IUserDAO;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.TypedQuery;
+
+/**
+ * Data Access Oject Class for the Users
+ */
+public class UserDAO implements IUserDAO {
+
+  private EntityManager getEntityManager() {
+    return JPAUtil.getEntityManager();
+  }
+
+  /**
+   * Creates a new user in the database
+   * 
+   * @param user User that is created in the database
+   */
+  @Override
+  public void create(User user) {
+    EntityManager em = getEntityManager();
+    EntityTransaction tx = em.getTransaction();
+    try {
+      tx.begin();
+      em.persist(user);
+      tx.commit();
+    } catch (Exception e) {
+      if (tx.isActive())
+        tx.rollback();
+      throw e;
+    } finally {
+      em.close();
+    }
+  }
+
+  /**
+   * Soft deletes a user from the system, disabling it but letting it in the
+   * database
+   * 
+   * @param id Id of the user to delete
+   */
+  @Override
+  public void delete(Integer id) {
+    // Soft delete (recomended for users): change isActive to false
+    EntityManager em = getEntityManager();
+    EntityTransaction tx = em.getTransaction();
+    try {
+      tx.begin();
+      User user = em.find(User.class, id);
+      if (user != null) {
+        user.setIsActive(false);
+        em.merge(user);
+      }
+      tx.commit();
+    } catch (Exception e) {
+      if (tx.isActive())
+        tx.rollback();
+      throw e;
+    } finally {
+      em.close();
+    }
+  }
+
+  /**
+   * Finds a user in the database via an id
+   * 
+   * @param id ID of the user to find
+   */
+  @Override
+  public User find(Integer id) {
+    EntityManager em = getEntityManager();
+    try {
+      return em.find(User.class, id);
+    } finally {
+      em.close();
+    }
+  }
+
+  /**
+   * Finds all the users in the database
+   */
+  @Override
+  public List<User> findAll() {
+    EntityManager em = getEntityManager();
+    try {
+      String jpql = "SELECT u FROM User u";
+      TypedQuery<User> query = em.createQuery(jpql, User.class);
+      return query.getResultList();
+    } finally {
+      em.close();
+    }
+  }
+
+  /**
+   * Updates a user from the database
+   * 
+   * @param user User that will be updated
+   */
+  @Override
+  public void update(User user) {
+    EntityManager em = getEntityManager();
+    EntityTransaction tx = em.getTransaction();
+    try {
+      tx.begin();
+      em.merge(user);
+      tx.commit();
+    } catch (Exception e) {
+      if (tx.isActive())
+        tx.rollback();
+      throw e;
+    } finally {
+      em.close();
+    }
+  }
+
+}
