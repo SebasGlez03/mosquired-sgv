f5ab903398abf842585cf7fc3356a0c4b9f380cc
SebasGlez03 <sebas.glez18040465@gmail.com>
2025-12-27 18:57:14 -0700

    Implementation of ClientDAO
    Completion of the implementation of all the implemented methods from the
    IClientDAO class

--- MosquiredSGV/SGV_Persistence/src/main/java/com/persistence/impl/ClientDAO.java
+++ MosquiredSGV/SGV_Persistence/src/main/java/com/persistence/impl/ClientDAO.java
@@ -3,44 +3,132 @@ package com.persistence.impl;
 import java.util.List;
 
 import com.domain.Client;
+import com.domain.util.JPAUtil;
 import com.persistence.IClientDAO;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.TypedQuery;
 
 public class ClientDAO implements IClientDAO {
 
+  private EntityManager getEntityManager() {
+    return JPAUtil.getEntityManager();
+  }
+
+  /**
+   * Creates a client in the database
+   * 
+   * @param client Client to add in the database
+   */
   @Override
   public void create(Client client) {
-    // TODO Auto-generated method stub
-
+    EntityManager em = getEntityManager();
+    EntityTransaction tx = em.getTransaction();
+    try {
+      tx.begin();
+      em.persist(client);
+      tx.commit();
+    } catch (Exception e) {
+      if (tx.isActive())
+        tx.rollback();
+      throw e;
+    } finally {
+      em.close();
+    }
   }
 
+  /**
+   * Deletes a client from the database
+   * 
+   * @param id ID of the client to delete
+   */
   @Override
   public void delete(Integer id) {
-    // TODO Auto-generated method stub
+    EntityManager em = getEntityManager();
+    EntityTransaction tx = em.getTransaction();
+    try {
+      tx.begin();
+      Client client = em.find(Client.class, id);
+      if (client != null) {
+        em.remove(client);
+      }
+      tx.commit();
+    } catch (Exception e) {
+      if (tx.isActive())
+        tx.rollback();
+    } finally {
+      em.close();
+    }
 
   }
 
+  /**
+   * Finds a client via a given ID
+   * 
+   * @param id ID of the client to find
+   */
   @Override
   public Client find(Integer id) {
-    // TODO Auto-generated method stub
-    return null;
+    EntityManager em = getEntityManager();
+    try {
+      return em.find(Client.class, id);
+    } finally {
+      em.close();
+    }
   }
 
+  /**
+   * Finds all the list of clients
+   */
   @Override
   public List<Client> findAll() {
-    // TODO Auto-generated method stub
-    return null;
+    EntityManager em = getEntityManager();
+    try {
+      return em.createQuery("SELECT c FROM Client c", Client.class).getResultList();
+    } finally {
+      em.close();
+    }
   }
 
+  /**
+   * Finds a list of clients by the name
+   * 
+   * @param name Name of the clients to find
+   */
   @Override
   public List<Client> findByName(String name) {
-    // TODO Auto-generated method stub
-    return null;
+    EntityManager em = getEntityManager();
+    try {
+      String jpql = "SELECT c FROM Client c WHERE c.name LIKE :name";
+      TypedQuery<Client> query = em.createQuery(jpql, Client.class);
+      query.setParameter("name", "%" + name + "%");
+      return query.getResultList();
+    } finally {
+      em.close();
+    }
   }
 
+  /**
+   * Updates a client from the database
+   * 
+   * @param client Client to update
+   */
   @Override
   public void update(Client client) {
-    // TODO Auto-generated method stub
-
+    EntityManager em = getEntityManager();
+    EntityTransaction tx = em.getTransaction();
+    try {
+      tx.begin();
+      em.merge(client);
+      tx.commit();
+    } catch (Exception e) {
+      if (tx.isActive())
+        tx.rollback();
+      throw e;
+    } finally {
+      em.close();
+    }
   }
 
 }
